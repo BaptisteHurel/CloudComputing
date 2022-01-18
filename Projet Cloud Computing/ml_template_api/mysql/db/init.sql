47afbc7569038a34b5a49d9b1fea1b5ca48e9899
Baptiste Hurel <58144828+BaptisteHurel@users.noreply.github.com>
2022-01-18 18:19:09 +0100

    re upload

---
+++ Projet Cloud Computing/ml_template_api/mysql/db/init.sql
@@ -0,0 +1,44 @@
+/*
+*********************************************************************
+http://www.mysqltutorial.org
+*********************************************************************
+Name: MySQL Sample Database classicmodels
+Link: http://www.mysqltutorial.org/mysql-sample-database.aspx
+Version 3.1
++ changed data type from DOUBLE to DECIMAL for amount columns
+Version 3.0
++ changed DATETIME to DATE for some colunmns
+Version 2.0
++ changed table type from MyISAM to InnoDB
++ added foreign keys for all tables 
+*********************************************************************
+*/
+
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`dbconnect` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `dbconnect`;
+
+/*Table structure for table `customers` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`),
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `customers` */
+
+insert into `user`(`id`,`password`
+)
+(1,'hurel123')
